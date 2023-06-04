8f552b14d99a7441781b0dd829eba17fef707f57
xuhuafan <122888182+xuhuafan@users.noreply.github.com>
2023-06-04 17:59:57 +0800

    Update JDINAC_m.R

--- R/JDINAC_m.R
+++ R/JDINAC_m.R
@@ -1,4 +1,4 @@
-#' @description function jdinac.m: perform JDINAC with bivariate kernel density estimation using bandwidth matrix
+#' @description function jdinac.m: perform JDINAC with bivariate kernel density estimation using bandwidth matrix.
 #'
 #' @param h0: bandwidth matrix list for class 0, consisting of bandwidth matrixs for each edge (interaction) in EDGE.
 #' @param h1: bandwidth matrix list for class 1, consisting of bandwidth matrixs for each edge (interaction) in EDGE.
@@ -76,8 +76,7 @@ jdinac.m <- function(h0,h1,EDGE,classLabel,DataFit,DataPre,nsplit=10,nfolds=5){
       index1=which(classLabel==1)
       sn0 <- round(size0/2)
       sn1 <- round(size1/2)
-      
-      #set.seed(10*i+456)
+      set.seed(10*i+456)
       splitid = c(sample(index0,sn0),sample(index1,sn1))
       cLabel <- classLabel[splitid]   
       denX=matrix(nrow=nrow(rbind(DataFit[-splitid,],DataPre)),ncol=0)
@@ -125,13 +124,69 @@ jdinac.m <- function(h0,h1,EDGE,classLabel,DataFit,DataPre,nsplit=10,nfolds=5){
     Vid <- as.numeric(rownames(numb))  
     Eset <- cbind(EDGE[Vid,],as.numeric(numb))  
     Eset <- Eset[order(Eset[,3],decreasing=T),]  
-    colnames(Eset) <- c("row","col","numb")
-    
+    colnames(Eset) <- c("row","col","numb")    
     Vars=cbind(vars,EDGE[vars[,1],])
     Vars=Vars[,-1]
-    colnames(Vars)=c("t","coef","row","col")
-    
+    colnames(Vars)=c("t","coef","row","col")    
   } 
   list(yPre=yPre,Eset=Eset,Vars=Vars,preY=preY) 
+}
+
+jdinac_md=function(y1,y2,data,dfh){
+  pdim=870
+  train=data[data$y %in% c(y1,y2) & data$type=="train",1:(pdim+1)]
+  test=data[data$y %in% c(y1,y2) & data$type=="test",1:(pdim+1)]
+  index=rep(0,pdim)
+  for(i in 1:pdim){index[i]=as.numeric(substring(colnames(data)[i],2,))}
+  dh=colSums(dfh[dfh[,1] %in% c(y1,y2) | dfh[,2] %in% c(y1,y2),-c(1,2)])
+  vs=index[which(dh>=1)]
+  vs=vs[vs<4000]
+  p=length(vs)
+  x_ind=ifelse(index %in% vs,1,0)  ## feature index selected by HC
+  x_ind[pdim+1]=1
+  E=diag(0,p)
+  colnames(E)=vs
+  rownames(E)=vs
+  f_same=function(vs,x,num){row=ifelse(vs %in% x,num,0)}
+  pvc=c(600:750,1076:1116,1183:1223,1232:1272,1310:1350,1409:1449,2830:2870,2904:2944,2948:2988)
+  caco3=c(698:732,833:896,1064:1104,1420:1460,1776:1816,2493:2533,2855:2895,2962:3002)
+  PAEs=c(685:790,1020:1093,1104:1144,1266:1306,1361:1401,1442:1482,1560:1620,1710:1750,2843:2894,2912:2981,3017:3057,3417:3457)
+  E[f_same(vs,pvc,1)==1,f_same(vs,pvc,1)==1]=1
+  E[f_same(vs,caco3,2)==2,f_same(vs,caco3,2)==2]=1
+  E[f_same(vs,PAEs,3)==3,f_same(vs,PAEs,3)==3]=1
+  E[lower.tri(E,diag=T)]=0
+  EDGE=which(E!=0, arr.ind=T)
+  sam_train=train[,x_ind==1]
+  sam_test=test[,x_ind==1]
+  classlabel_train=ifelse(sam_train$y %in% y1,0,1)
+  classlabel_test=ifelse(sam_test$y %in% y1,0,1)
+  datafit=sam_train[,-ncol(sam_train)]
+  datapre=sam_test[,-ncol(sam_test)]
+  joint_bw0=list()
+  joint_bw1=list()
+  for(e in 1:nrow(EDGE)){
+    rnum=EDGE[e,1];cnum=EDGE[e,2]
+    edt0=sam_0[,c(rnum,cnum)]
+    bw0=Hns(edt0)
+    joint_bw0[[e]]=bw0
+    edt1=sam_1[,c(rnum,cnum)]
+    bw1=Hns(edt1)
+    joint_bw1[[e]]=bw1
+  }
+  difnet=jdinac.m(h0=joint_bw0,h1=joint_bw1,EDGE=EDGE,classLabel=classlabel_train,DataFit=datafit,DataPre=datapre,nsplit=10,nfolds=5)
+  ypre=ifelse(difnet$yPre>0.5,1,0) ##predicted class
+  err=mean(ypre!=classlabel_test)   #classification error
+  ypre=data.frame(ypre)
+  ypre['true_class']=classlabel_test
+  ypre['label']=sam_test$y
+  ypre=cbind(ypre,difnet$yPre,difnet$preY)
+  Vars=data.frame(difnet$Vars)  #coefficients
+  Vars['v1']=vs[Vars[,3]]
+  Vars['v2']=vs[Vars[,4]]
+  eset=difnet$Eset  #differential interaction
+  eset=data.frame(eset)
+  eset['v1']=vs[eset[,1]]
+  eset['v2']=vs[eset[,2]]
+  list(err=err,eset=eset)
 }
 
